f675723d1bc05a6b899e1dccb0d13e753759ce3c
jaseembinmuhammed@gmail.com <jaseembinmuhammed@gmail.com>
2025-03-10 01:22:51 +0530

    added material angular

--- src/app/screen/login/login.component.ts
+++ src/app/screen/login/login.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
