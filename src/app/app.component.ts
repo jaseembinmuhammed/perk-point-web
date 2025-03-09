0010d8d4934e26f22751e72843292198fdde5302
jaseembinmuhammed@gmail.com <jaseembinmuhammed@gmail.com>
2025-03-10 00:38:21 +0530

    title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './screen/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'perk-point';
+  title = 'Bismillah';
 }
