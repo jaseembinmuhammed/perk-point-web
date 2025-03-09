8f09446f466b12550d2bc7c053c9e5724ffb91f4
jaseembinmuhammed@gmail.com <jaseembinmuhammed@gmail.com>
2025-03-10 02:57:49 +0530

    login completed

--- src/app/screen/login/login.component.ts
+++ src/app/screen/login/login.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule,MatIconModule,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  hide = true;
+
+  onEditClick() {
+    alert('Edit icon clicked!');
+    // Add your custom logic here
+  }
 
 }
