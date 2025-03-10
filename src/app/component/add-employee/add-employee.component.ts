74cfee78b69987461615b5a0e637b35a6dd55c90
jaseembinmuhammed@gmail.com <jaseembinmuhammed@gmail.com>
2025-03-10 23:01:16 +0530

    new employee modal

--- src/app/component/add-employee/add-employee.component.ts
+++ src/app/component/add-employee/add-employee.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-add-employee',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatProgressBarModule,
+  ],
   templateUrl: './add-employee.component.html',
-  styleUrl: './add-employee.component.scss'
+  styleUrl: './add-employee.component.scss',
 })
 export class AddEmployeeComponent {
+  readonly dialogRef = inject(MatDialogRef<AddEmployeeComponent>);
+  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+  readonly animal = model(this.data.animal);
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
+
+export interface DialogData {
+  animal: string;
+  name: string;
 }
--- src/app/screens/employee-list/employee-list.component.ts
+++ src/app/screens/employee-list/employee-list.component.ts
@@ -1,41 +1,74 @@
-import {AfterViewInit, Component, inject, OnInit, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  model,
+  OnInit,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { EmployeeService } from '../../services/employee.service';
 import { catchError, of } from 'rxjs';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialog } from '@angular/material/dialog';
+import { AddEmployeeComponent } from '../../component/add-employee/add-employee.component';
 
 /**
  * @title Table with pagination
  */
 @Component({
   selector: 'app-employee-list',
-  imports: [MatTableModule,MatPaginatorModule,MatButtonModule,MatTooltipModule],
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
   templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.scss'
+  styleUrl: './employee-list.component.scss',
 })
-export class EmployeeListComponent  implements OnInit {
-
+export class EmployeeListComponent implements OnInit {
   employeeService = inject(EmployeeService);
-
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource<Todo>([]);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
- 
-  ngOnInit(): void {
-    this.employeeService.getEmployeeList().pipe( catchError(err=>{
-      console.log(err);
-      return of([]); 
-    })).subscribe(list=>{
-      this.dataSource = new MatTableDataSource<Todo>(list);
-      this.dataSource.paginator = this.paginator;
-      console.log(list);
-    })
+
+  readonly animal = signal('');
+  readonly name = model('');
+  readonly dialog = inject(MatDialog);
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddEmployeeComponent, {
+      data: { name: this.name(), animal: this.animal() },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed', result);
+      if (result !== undefined) {
+        this.animal.set(result);
+      }
+    });
   }
 
-  
+  ngOnInit(): void {
+    this.employeeService
+      .getEmployeeList()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          return of([]);
+        })
+      )
+      .subscribe((list) => {
+        this.dataSource = new MatTableDataSource<Todo>(list);
+        this.dataSource.paginator = this.paginator;
+        console.log(list);
+      });
+  }
 }
 
 interface Todo {
@@ -53,24 +86,24 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
+  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
+  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
+  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
+  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
+  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
+  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
+  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
+  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
+  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
 ];
