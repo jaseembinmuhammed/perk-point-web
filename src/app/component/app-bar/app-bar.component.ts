89be633f7d99433b6c2e3cff69d1c4a48fe008af
jaseembinmuhammed@gmail.com <jaseembinmuhammed@gmail.com>
2025-03-12 02:56:37 +0530

    my rewards css

--- src/app/component/app-bar/app-bar.component.ts
+++ src/app/component/app-bar/app-bar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthResponse } from '../../services/auth.service';
 
 @Component({
   selector: 'app-app-bar',
@@ -9,11 +10,26 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './app-bar.component.html',
   styleUrl: './app-bar.component.scss',
 })
-export class AppBarComponent {
+export class AppBarComponent implements OnInit {
   constructor(private router: Router) {}
+
+  showAdminMenu = false;
+  showEmployeeMenu = false;
 
   onLogout() {
     localStorage.removeItem('authToken');
     this.router.navigate(['/']);
+  }
+
+  ngOnInit(): void {
+    const authData = localStorage.getItem('authToken');
+    if (authData) {
+      let userInfo: AuthResponse = JSON.parse(authData);
+      if (userInfo?.principal?.roles?.includes('ADMIN')) {
+        this.showAdminMenu = true;
+      } else if (userInfo?.principal?.roles?.includes('EMPLOYEE')) {
+        this.showEmployeeMenu = true;
+      }
+    }
   }
 }
